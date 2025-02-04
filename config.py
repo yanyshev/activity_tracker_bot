ec6463d72e647a0eb1a6ac2c9d04bb17ef608386
Dmitry Yanyshev <dima.yanyshev@icloud.com>
2025-02-04 10:19:17 +0300

    Update

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
 if not TOKEN:
     raise ValueError("BOT_TOKEN is not set!")
+
+WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
+if not WEATHER_API_KEY:
+    raise ValueError("WEATHER_API_KEY is not set!")
